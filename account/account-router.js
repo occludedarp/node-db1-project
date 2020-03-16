2c107b6c1c8709113ca41fe0d36f59819d079f39
Cameron Moore <cmrnmr@gmail.com>
2020-03-16 17:06:34 -0400

    server running, SQL questions done

---
+++ account/account-router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const db = require('../data/dbConfig.js');
+
+const router = express.Router();
+
+// router.get( '/', (req, res) => {
+//   db.
+// })
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js");
+const AccountRouter = require('../account/account-router.js');
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', AccountRouter);
+
+server.get('/', (req, res) => {
+  res.status(200).json({"api": "up and running"})
+})
 
 module.exports = server;
